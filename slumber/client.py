de8e8de910a6f2e4cb76cdb4cc28862b6c71c3ed
juacompe <juacompe@squire.proteus-tech.com>
2011-07-23 01:50:13 +0700

    done load_apps and load_models, about to refector load models to use sub client

--- slumber/client.py
+++ slumber/client.py
@@ -5,20 +5,46 @@ class Client(object):
     def __init__(self, server='localhost', root='', protocol='http'):
         self.protocol = protocol 
         self.server = server
-        self.root = root
         self.http = Http()
 
-        self.slumber_test = True 
-        self.django_contrib_messages = True
+        self._load_apps(root)
 
     def _do_get(self, uri, query={}):
         """
         get response in JSON format from slumber server and loads it into a python dict
         """ 
         url = self._get_url(uri) 
+        print 'url = ', url
         request, content = self.http.request(url)
         return request, loads(content)
         
     def _get_url(self, uri='/'):
         server = self.protocol + '://' + self.server
-        return  server + self.root + uri
+        return  server + uri
+
+    def _load_apps(self, url):
+        print 'in _load_apps'
+        response, json = self._do_get(url)
+        apps = json['apps']
+
+        for key, value in apps.items():
+            models_url = value
+            field_name = key.replace('.', '_')
+            setattr(self, field_name, MockedModel())
+            self._load_models(getattr(self, field_name), models_url)
+        print 'exit _load_apps'
+
+    def _load_models(self, app, url):
+        print 'in _load_models'
+        response, json = self._do_get(url)
+        models = json['models']
+        
+        for key, value in models.items():
+            model_url = value
+            field_name = key.replace('.', '_')
+            setattr(app, field_name, value)
+
+        print 'exit _load_models'
+
+class MockedModel(object):
+    pass
--- slumber_test/tests/test_client.py
+++ slumber_test/tests/test_client.py
@@ -1,38 +1,64 @@
 from django.test import TestCase
 from slumber.client import Client
+from slumber_test.models import Pizza
+from mock import patch
 
-class TestClient(TestCase):
-        
-    def test_get_default_url(self):
+class TestGetUrl(TestCase):
+    @patch('slumber.client.Client._load_apps') 
+    def test_get_default_url(self, mocked_load_apps):
         client = Client()
         self.assertEqual('http://localhost/', client._get_url())
 
-    def test_get_url_of_google(self):
+    @patch('slumber.client.Client._load_apps') 
+    def test_get_url_of_google(self, mocked_load_apps):
         client = Client('www.google.com')
         self.assertEqual('http://www.google.com/', client._get_url())
         
-    def test_get_url_with_root(self):
-        client = Client(root='/slumber')
-        self.assertEqual('http://localhost/slumber/', client._get_url())
-
-    def test_get_some_url(self):
-        client = Client(root='/slumber')
-        self.assertEqual('http://localhost/slumber/slumber_test', client._get_url('/slumber_test'))
-
-    def test_applications(self):
+    @patch('slumber.client.Client._load_apps') 
+    def test_get_url_with_root(self, mocked_load_apps):
         client = Client()
-        self.assertTrue(client.slumber_test)
-        self.assertTrue(client.django_contrib_messages)
+        self.assertEqual('http://localhost/', client._get_url())
 
+    @patch('slumber.client.Client._load_apps') 
+    def test_get_some_url(self, mocked_load_apps):
+        client = Client()
+        self.assertEqual('http://localhost/slumber_test/', client._get_url('/slumber_test/'))
+        
 
 class TestDoGet(TestCase):
-
-    def test_do_get(self):
+    @patch('slumber.client.Client._load_apps') 
+    def test_do_get(self, mocked_load_apps):
         """
         _do_get should return the dict of the result
         """
-        client = Client('localhost:8003', '/slumber')
-        response, json = client._do_get('/')
+        client = Client('localhost:8003')
+        response, json = client._do_get('/slumber')
         apps = json['apps']
         self.assertEquals(apps['slumber_test'], '/slumber/slumber_test/')
+
+
+class TestLoads(TestCase):
+    def test_applications(self):
+        client = Client('localhost:8003', '/slumber')
+        self.assertTrue(client.slumber_test)
+
+    @patch('slumber.client.Client._load_apps') 
+    def test_root_is_passed_in_load_apps(self, mocked_load_apps):
+        client = Client(root='/slumber')
+        args = (('/slumber',))
+        mocked_load_apps.assert_called_with_args(args)
+
+    def test_applications_with_dots_in_name(self):
+        """
+        dots (.) will be replaced with underscores (_) for some apps that may have dots in its name 
+        (i.e. django.contrib.messages)
+        """
+        client = Client('localhost:8003', '/slumber')
+        self.assertTrue(client.django_contrib_messages)
+
+    def test_instance_date(self):
+        s = Pizza(name='S1', for_sale=True)
+        s.save()
+        client = Client('localhost:8003', '/slumber')
+        pizza = client.slumber_test.Pizza
 
