6753f4a3520e44e656f51ad7de9971761615f76a
Kirit Sælensminde <k@kirit.com>
2011-07-26 23:09:34 +0700

    Split out the dict object classes.

--- slumber/connector/__init__.py
+++ slumber/connector/__init__.py
@@ -6,33 +6,12 @@ from urlparse import urljoin
 from httplib2 import Http
 from simplejson import loads
 
+from slumber.connector.dictobject import DictObject, LazyDictObject
 from slumber.json import from_json_data
 
 
 _fake = FakeClient()
 _http = Http()
-
-
-class DictObject(object):
-    """Allows generic Python objects to be created from a nested dict
-    structure describing the attrbutes.
-    """
-
-    def __init__(self, **kwargs):
-        """Load the specified key values.
-        """
-        for k, v in kwargs.items():
-            setattr(self, k, v)
-
-class LazyDictObject(DictObject):
-    """Allows generic Python objects to be created lazily when attributes are requested.
-    """
-    def __init__(self, getattr_function, **kwargs):
-        self._getattr = getattr_function
-        super(LazyDictObject, self).__init__(**kwargs)
-
-    def __getattr__(self, name):
-        return self._getattr(self, name)
 
 
 def get(url, query={}):
