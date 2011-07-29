b948a796c51f253a7ac67ad69b8a5e786c754a13
Kirit Sælensminde <k@kirit.com>
2011-07-29 13:46:01 +0700

    Added some more documentation.

--- slumber/__init__.py
+++ slumber/__init__.py
@@ -1,3 +1,7 @@
+"""
+    Slumber is a RESTful client/server implementation that makes it simple
+    to make use of Django models in a RESTful manner.
+"""
 from slumber.connector import Client
 
 # In order to allow mocking of the Slumber client instance we need an extra
@@ -7,11 +11,14 @@ from slumber.connector import Client
 # Create a location for where to put the real client
 _client = Client()
 
-# Create a class that just forwards to the real client
 class _ClientProxy(object):
+    """A forwarder for the real client.
+    """
     def __getattr__(self, name):
         return getattr(_client, name)
 
 # The client other code will import will always forward to the mockable
 # _client instance.
+# This is exactly the name we want to use here
+# pylint: disable=C0103
 client = _ClientProxy()
