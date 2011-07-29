cf5ca99b50db67bee30fe61b7e48b9c00ab069d7
Kirit Sælensminde <k@kirit.com>
2011-07-29 12:41:52 +0700

    Cleaned up another file for pylint.

--- slumber/connector/__init__.py
+++ slumber/connector/__init__.py
@@ -1,3 +1,6 @@
+"""
+    Code for the Slumber client connector.
+"""
 from django.conf import settings
 
 from urllib import urlencode
@@ -9,6 +12,8 @@ from slumber.json import from_json_data
 
 
 class Client(object):
+    """The first level of the Slumber client connector.
+    """
     def __init__(self, directory=None):
         if not directory:
             directory = getattr(settings, 'SLUMBER_DIRECTORY',
@@ -16,7 +21,9 @@ class Client(object):
         self._directory = directory
 
     def __getattr__(self, attr_name):
-        response, json = get(self._directory)
+        """Fetch the application list from the Slumber directory on request.
+        """
+        _, json = get(self._directory)
         apps = {}
         for app in json['apps'].keys():
             root = apps
@@ -25,9 +32,12 @@ class Client(object):
                     root[k] = {}
                 root = root[k]
         def recurse_apps(loc, this_level, name):
+            """Recursively build the application connectors.
+            """
             current_appname = '.'.join(name)
             if json['apps'].has_key(current_appname):
-                loc._url = urljoin(self._directory, json['apps'][current_appname])
+                loc._url = urljoin(self._directory,
+                    json['apps'][current_appname])
             for k, v in this_level.items():
                 app_cnx = AppConnector()
                 setattr(loc, k, app_cnx)
@@ -40,32 +50,38 @@ class Client(object):
 
 
 class AppConnector(DictObject):
+    """Handles the client connection to a Django application.
+    """
     _url = None
     def __getattr__(self, name):
         if not self._url:
             raise AttributeError(name)
-        response, json = get(self._url)
+        _, json = get(self._url)
         models = json['models']
-        for m, u in models.items():
-            model_url = urljoin(self._url, u)
-            setattr(self, m, ModelConnector(model_url))
+        for model, url in models.items():
+            model_url = urljoin(self._url, url)
+            setattr(self, model, ModelConnector(model_url))
         if name in models.keys():
             return getattr(self, name)
         else:
             raise AttributeError(name)
 
 
-def _return_data_array(base_url, arrays, instance, name):
+def _return_data_array(base_url, arrays, _, name):
+    """Implement the lazy fetching of the instance data.
+    """
+    # Pylint makes a bad type deduction
+    # pylint: disable=E1103
     if name in arrays.keys():
         data_array = []
-        response, data = get(urljoin(base_url, arrays[name]))
+        _, data = get(urljoin(base_url, arrays[name]))
         while True:
             for obj in data['page']:
                 data_array.append(
                     InstanceConnector(
                         urljoin(base_url, obj['data']), obj['display']))
             if data.has_key('next_page'):
-                response, data = get(urljoin(base_url, data['next_page']))
+                _, data = get(urljoin(base_url, data['next_page']))
             else:
                 break
         return data_array
@@ -73,23 +89,33 @@ def _return_data_array(base_url, arrays, instance, name):
         raise AttributeError(name)
 
 class ModelConnector(DictObject):
-    def __init__(self, url):
+    """Handles the connection to a Django model.
+    """
+    def __init__(self, url, **kwargs):
         self._url = url
+        super(ModelConnector, self).__init__(**kwargs)
 
     def get(self, **kwargs):
+        """Implements the client side for the model 'get' operator.
+        """
         pk = kwargs.get('pk', None)
         if pk is None:
             return None
         url = urljoin(self._url, 'get/')
-        response, json = get(url + '?' + urlencode({'pk': pk}))
+        _, json = get(url + '?' + urlencode({'pk': pk}))
         def get_data_array(obj, name):
+            """Implement simple partial application using a closure.
+            """
             return _return_data_array(self._url, json['data_arrays'], obj, name)
         obj = LazyDictObject(get_data_array,
-            **dict([(k, from_json_data(self._url, j)) for k, j in json['fields'].items()]))
+            **dict([(k, from_json_data(self._url, j))
+                for k, j in json['fields'].items()]))
         return obj
 
 
 class InstanceConnector(DictObject):
+    """Connects to a remote instance.
+    """
     def __init__(self, url, display, **kwargs):
         self._url = url
         self._unicode = display
@@ -99,7 +125,7 @@ class InstanceConnector(DictObject):
         return self._unicode
 
     def __getattr__(self, name):
-        response, json = get(self._url)
+        _, json = get(self._url)
         for k, v in json['fields'].items():
             setattr(self, k, from_json_data(self._url, v))
         if name in json['fields'].keys():
