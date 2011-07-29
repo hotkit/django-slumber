e578ece6c4a9f67f97cbf49029bfb46ea2b6ed19
Kirit Sælensminde <k@kirit.com>
2011-07-29 13:04:02 +0700

    Cleaned up for pylint.

--- slumber/http.py
+++ slumber/http.py
@@ -1,12 +1,19 @@
+"""
+    Implements the conversion of the response data to valid HTTP
+    data.
+"""
 from simplejson import dumps
 
 from django.http import HttpResponse
 
 
 def view_handler(view):
-    """Wrap a view function so it can return either JSON, HTML or someother response.
+    """Wrap a view function so it can return either JSON, HTML or some
+    other response.
     """
     def wrapper(request, *args, **kwargs):
+        """The decorated implementation.
+        """
         response = {}
         http_response = view(request, response, *args, **kwargs)
         if http_response:
