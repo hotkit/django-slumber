9b55d4b3ed62373c229ab57cf8bc863194ef4a77
Kirit Sælensminde <k@kirit.com>
2011-08-05 10:57:49 +0700

    Moved the instance list operation into its own file.

--- slumber/operations/__init__.py
+++ slumber/operations/__init__.py
@@ -24,28 +24,6 @@ class InstanceOperation(ModelOperation):
         self.regex = '([^/]+)/'
 
 
-class InstanceList(ModelOperation):
-    """Allows access to the instances.
-    """
-    def operation(self, request, response, _appname, _modelname):
-        """Return a paged set of instances for this model.
-        """
-        root = reverse('slumber.server.views.get_applications')
-        response['model'] = root + self.model.path
-
-        query = self.model.model.objects.order_by('-pk')
-        if request.GET.has_key('start_after'):
-            query = query.filter(pk__lt=request.GET['start_after'])
-
-        response['page'] = [
-                dict(pk=o.pk, display=unicode(o),
-                    data=root + self.model.path + 'data/%s/' % o.pk)
-            for o in query[:10]]
-        if len(response['page']) > 0:
-            response['next_page'] = root +self.model.path + \
-                'instances/?start_after=%s' % response['page'][-1]['pk']
-
-
 class CreateInstance(ModelOperation):
     """Allows for the creation of new instances.
     """
--- slumber/server/model.py
+++ slumber/server/model.py
@@ -5,7 +5,8 @@ from django.db.models import ForeignKey
 from django.db.models.fields import FieldDoesNotExist
 
 from slumber._caches import MODEL_CACHE
-from slumber.operations import InstanceList, CreateInstance
+from slumber.operations import CreateInstance
+from slumber.operations.instancelist import InstanceList
 from slumber.operations.instancedata import DereferenceInstance, \
     InstanceData, InstanceDataArray
 from slumber.server import get_slumber_root
