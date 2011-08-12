2b77ce9b5f7333167e95ca8aa0ae12ae326c7eb8
Kirit Saelensminde <k@kirit.com>
2011-08-12 11:02:32 +0700

    Implemented a simple unicode wrapper around unknown types that end up in the JSON output from Slumber.

--- slumber/server/http.py
+++ slumber/server/http.py
@@ -2,9 +2,19 @@
     Implements the conversion of the response data to valid HTTP
     data.
 """
-from simplejson import dumps
+from simplejson import dumps, JSONEncoder
 
 from django.http import HttpResponse
+
+
+class _proxyEncoder(JSONEncoder):
+    """If we don't know how to deal with the attribute type we'll just
+    convert to a string and hope that's ok for now.
+    """
+    # An attribute inherited from JSONEncoder hide this method
+    # pylint: disable=E0202
+    def default(self, obj):
+        return unicode(obj)
 
 
 def view_handler(view):
@@ -18,6 +28,7 @@ def view_handler(view):
         http_response = view(request, response, *args, **kwargs)
         if http_response:
             return http_response
-        return HttpResponse(dumps(response, indent=4), 'text/plain',
+        return HttpResponse(dumps(response, indent=4,
+                cls=_proxyEncoder), 'text/plain',
             status=response['_meta']['status'])
     return wrapper
