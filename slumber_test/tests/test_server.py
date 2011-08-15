37a549d36ed11d7e323a7594e5c69c8aa5c41dfc
Kirit Saelensminde <k@kirit.com>
2011-08-15 21:15:49 +0700

    Fixed a test so it will pass on any day.

--- slumber_test/tests/test_server.py
+++ slumber_test/tests/test_server.py
@@ -6,9 +6,11 @@ from slumber.server.http import view_handler
 
 class TestJSON(TestCase):
     def test_unicode_attributes(self):
+        d = date.today()
         @view_handler
         def view(request, response):
-            response['u'] = date.today()
+            response['u'] = d
         http_response = view({})
         self.assertEquals(http_response.content,
-            """{\n    "u": "2011-08-11",\n    "_meta": {\n        "status": 200,\n        "message": "OK"\n    }\n}""")
+            """{\n    "u": "%s",\n    "_meta": {\n        "status": 200,\n        "message": "OK"\n    }\n}""" %
+                d)
