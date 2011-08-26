8b635eca669517abd51ddb034fd848c70605b102
Kirit Sælensminde <k@kirit.com>
2011-08-26 11:07:35 +0700

    Added threadlocal storage caches and added an instance cache for the client to use.

--- slumber/_caches.py
+++ slumber/_caches.py
@@ -1,5 +1,13 @@
 """
     Some caches used in the implementation of the Slumber client or server.
 """
+import threading
 
+# Allow some caches to be thread local
+_threadlocals = threading.local()
+
+# Stores the server model for a given Django model in the server
 MODEL_CACHE = {}
+
+# Stores instances in the client
+_threadlocals.INSTANCE_CACHE = {}
