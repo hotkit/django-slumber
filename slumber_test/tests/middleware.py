68d6f3892f80095a51210f466ac0979b56d5c493
Kirit Sælensminde <k@kirit.com>
2011-08-27 11:33:18 +0700

    The test projects now use the middleware and the test makes sure it flushes the cache.

--- slumber_test/tests/middleware.py
+++ slumber_test/tests/middleware.py
@@ -1,6 +1,13 @@
 from django.test import TestCase
 
+from mock import patch
+
 
 class TestMiddleware(TestCase):
     def test_request(self):
-        response = self.client.get('/')
+        called = []
+        def flush_cache(*a):
+            called.append(True)
+        with patch('slumber.connector.Client._flush_client_instance_cache', flush_cache):
+            response = self.client.get('/')
+        self.assertTrue(called)
