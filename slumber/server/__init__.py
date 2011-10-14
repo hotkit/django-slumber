0415df553516c61068b2b6c5c8cdc4524a08d268
Kirit Sælensminde <k@kirit.com>
2011-10-14 19:00:56 +0700

    Fixed up the root URL so that it takes into account the service.

--- slumber/server/__init__.py
+++ slumber/server/__init__.py
@@ -1,11 +1,16 @@
 """
     The standard Slumber server implementation.
 """
+from django.conf import settings
 from django.core.urlresolvers import reverse
 
 
 def get_slumber_root():
     """Returns the location of the Slumber on this server.
     """
-    return reverse('slumber.server.views.service_root')
-    #return reverse('slumber.server.views.get_applications')
+    root = reverse('slumber.server.views.service_root')
+    service = getattr(settings, 'SLUMBER_ROOT', None)
+    if service:
+        return '%s/%s' % (root, service)
+    else:
+        return root
