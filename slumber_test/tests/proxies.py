c8bb0e5f2d96327527f72bed1487dbc30863d7ef
Kirit Sælensminde <k@kirit.com>
2011-10-18 21:46:36 +0700

    The test now passes as the correct proxy mix-in is used.

--- slumber_test/tests/proxies.py
+++ slumber_test/tests/proxies.py
@@ -1,11 +1,37 @@
+from mock import patch
 from unittest2 import TestCase
 
-from slumber import client
+from django.conf import settings
+from django.contrib.auth.models import User
+
+from slumber.connector import Client
 from slumber.connector.proxies import UserProxy
 from slumber.test import mock_client
 
 
-class UserProxyTests(TestCase):
+class ConfigureSlumber(object):
+    def setUp(self):
+        self.user = User(username='test')
+        self.user.save()
+
+        self.assertFalse(hasattr(settings, 'SLUMBER_DIRECTORY'))
+        self.assertFalse(hasattr(settings, 'SLUMBER_SERVICE'))
+        service = lambda: 'auth'
+        directory = lambda: {
+            'auth': 'http://localhost:8000/slumber/auth/',
+        }
+        self.__patchers = [
+            patch('slumber.server.views.get_slumber_service', service),
+            patch('slumber.server.get_slumber_service', service),
+            patch('slumber.server.get_slumber_directory', directory),
+        ]
+        [p.start() for p in self.__patchers]
+        self.client = Client()
+    def tearDown(self):
+        [p.stop() for p in self.__patchers]
+
+
+class UserProxyTests(ConfigureSlumber, TestCase):
     def test_user_proxy_is_used(self):
-        user = client.auth.django.contrib.auth.User.get(username='test')
+        user = self.client.auth.django.contrib.auth.User.get(username='test')
         self.assertIn(UserProxy, type(user).__mro__)
