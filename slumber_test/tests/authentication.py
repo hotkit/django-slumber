b224d9a9714d30f2a92ec02c123f5cb4d6809e07
Kirit Sælensminde <k@kirit.com>
2011-10-18 09:42:38 +0700

    Added a test to make sure the middleware doesn't error out.

--- slumber_test/tests/authentication.py
+++ slumber_test/tests/authentication.py
@@ -1,14 +1,28 @@
+from mock import patch
+
+from django.conf import settings
+from django.http import HttpResponse
 from django.test import TestCase
 
 
 class ConfigureAuthn(object):
     def setUp(self):
+        settings.MIDDLEWARE_CLASSES.append(
+            'slumber.connector.middleware.Authentication')
         super(ConfigureAuthn, self).setUp()
 
     def tearDown(self):
         super(ConfigureAuthn, self).tearDown()
+        settings.MIDDLEWARE_CLASSES.remove(
+            'slumber.connector.middleware.Authentication')
 
 
 class AuthenticationTests(ConfigureAuthn, TestCase):
-    def test_middleware(self):
-        pass
+    def test_no_middleware_error(self):
+        called = []
+        def view(request):
+            called.append(True)
+            return HttpResponse('ok')
+        with patch('slumber_test.views.ok_text', view):
+            self.client.get('/')
+        self.assertEqual(called, [True])
