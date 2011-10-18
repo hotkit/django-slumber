e8fd011774954c365ad564c115bc4b17f53aacf0
Kirit Sælensminde <k@kirit.com>
2011-10-18 21:31:18 +0700

    We can't use the mock client for testing proxies as they aren't part of the mock client.

--- slumber_test/tests/proxies.py
+++ slumber_test/tests/proxies.py
@@ -6,11 +6,6 @@ from slumber.test import mock_client
 
 
 class UserProxyTests(TestCase):
-    @mock_client(
-        auth__django__contrib__auth__User = [
-            dict(username='test')
-        ]
-    )
     def test_user_proxy_is_used(self):
         user = client.auth.django.contrib.auth.User.get(username='test')
-        self.assertIn(UserProxy, user.__bases__)
+        self.assertIn(UserProxy, type(user).__mro__)
