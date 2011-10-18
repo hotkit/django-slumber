a3e96a56d6c882ea63d3616fac264d7505be4991
Kirit Saelensminde <k@kirit.com>
2011-10-18 16:12:53 +0700

    Added empty test class for the proxy.

--- slumber_test/tests/__init__.py
+++ slumber_test/tests/__init__.py
@@ -2,6 +2,7 @@ from authentication import *
 from client import *
 from middleware import *
 from mock_client import *
+from proxies import *
 from remote_authn import *
 from server import *
 from services import *
