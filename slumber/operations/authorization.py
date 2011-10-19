d65470ecc0f72c59469b97cafc805f832eacd875
Kirit Sælensminde <k@kirit.com>
2011-10-19 08:26:38 +0700

    Added a wrapper for the module permissions API.

--- slumber/operations/authorization.py
+++ slumber/operations/authorization.py
@@ -15,6 +15,17 @@ class PermissionCheck(InstanceOperation):
         response['is-allowed'] = user.has_perm(permission)
 
 
+class ModulePermissions(InstanceOperation):
+    """Allows us to check if the user has any permissions for the module.
+    """
+
+    def get(self, _request, response, _appname, _modelname, pk, module):
+        """Implements the wrapper.
+        """
+        user = self.model.model.objects.get(pk=pk)
+        response['has_module_perms'] = user.has_module_perms(module)
+
+
 class GetPermissions(InstanceOperation):
     """Exposes the get_group_permissions API on Users.
     """
@@ -24,3 +35,4 @@ class GetPermissions(InstanceOperation):
         """
         user = self.model.model.objects.get(pk=pk)
         response['group_permissions'] = list(user.get_group_permissions())
+
