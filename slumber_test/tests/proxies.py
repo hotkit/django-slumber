03843ba901d7844f93e6136009844ca088b1e865
Kirit Sælensminde <k@kirit.com>
2011-10-19 09:38:16 +0700

    Refactored some more tests to remove duplication.

--- slumber_test/tests/proxies.py
+++ slumber_test/tests/proxies.py
@@ -6,25 +6,24 @@ from slumber_test.tests.configurations import PatchForAuthnService
 
 
 class UserProxyTests(PatchForAuthnService, TestCase):
+    def setUp(self):
+        super(UserProxyTests, self).setUp()
+        self.proxy_user = client.auth.django.contrib.auth.User.get(username='test')
+
     def test_user_proxy_is_used(self):
-        user = client.auth.django.contrib.auth.User.get(username='test')
-        self.assertIn(UserProxy, type(user).__mro__)
+        self.assertIn(UserProxy, type(self.proxy_user).__mro__)
 
     def test_has_perm(self):
-        user = client.auth.django.contrib.auth.User.get(username='test')
-        self.assertFalse(user.has_perm('slumber_test.not-a-permission'))
+        self.assertFalse(self.proxy_user.has_perm('slumber_test.not-a-permission'))
 
     def test_user_has_module_permission(self):
-        user = client.auth.django.contrib.auth.User.get(username='test')
-        self.assertFalse(user.has_module_perms('slumber_test'))
+        self.assertFalse(self.proxy_user.has_module_perms('slumber_test'))
 
     def test_user_get_group_permissions(self):
-        user = client.auth.django.contrib.auth.User.get(username='test')
-        perms = user.get_group_permissions()
+        perms = self.proxy_user.get_group_permissions()
         self.assertEqual(self.user.get_group_permissions(), perms)
 
     def test_user_get_all_permissions(self):
-        user = client.auth.django.contrib.auth.User.get(username='test')
-        perms = user.get_all_permissions()
+        perms = self.proxy_user.get_all_permissions()
         self.assertEqual(self.user.get_all_permissions(), perms)
 
