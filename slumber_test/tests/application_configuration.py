aeca968b9548d94abfb0196188a0bc84709fbc0c
Kirit Saelensminde <k@kirit.com>
2012-01-11 16:52:41 +0700

    Added a new class in a nested Django application which can be used to work on the slumber application configuration.

--- slumber_test/tests/application_configuration.py
+++ slumber_test/tests/application_configuration.py
@@ -1,12 +1,10 @@
 from django.test import TestCase
 
 from slumber import client
-from slumber.server.application import DjangoApp
 
 
 class TestNested1Configuration(TestCase):
     def test_nest_class_appears(self):
-        app = DjangoApp('slumber_test.nested1')
-        self.assertTrue(hasattr(client.shop, 'Nest'),
-            client.shop.__dict__.keys())
+        self.assertTrue(hasattr(client.slumber_test.nested1, 'Nest'),
+            client.slumber_test.nested1.__dict__.keys())
 
