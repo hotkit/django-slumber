84796427bb24918201b7b88a6b326a5305b6f4fc
Kirit Sælensminde <k@kirit.com>
2012-01-12 12:46:13 +0700

    Updated for new location of configuration.

--- slumber_examples/tests/application_configuration.py
+++ slumber_examples/tests/application_configuration.py
@@ -4,9 +4,8 @@ from slumber import client
 from slumber.server.application import DjangoApp
 
 
-class TestNested1Configuration(TestCase):
-    def test_nest_class_appears(self):
-        app = DjangoApp('slumber_test.nested1')
-        self.assertTrue(hasattr(client.shop, 'Nest'),
-            client.shop.__dict__.keys())
+class TestSlumberConfiguration(TestCase):
+    def test_shop_class_appears(self):
+        self.assertTrue(hasattr(client.slumber_examples, 'Shop'),
+            client.slumber_examples.__dict__.keys())
 
