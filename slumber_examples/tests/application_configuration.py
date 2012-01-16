c03f200c4ef8e8aba143d607c37602804d7c39f9
Kirit Sælensminde <k@kirit.com>
2012-01-16 12:03:50 +0700

    The Slumber configuration now also allows us to add in JSON transform functions for specific types for when property values are processed.

--- slumber_examples/tests/application_configuration.py
+++ slumber_examples/tests/application_configuration.py
@@ -1,6 +1,6 @@
 from django.test import TestCase
 
-from slumber import client
+from slumber import client, configure
 from slumber.server.application import DjangoApp
 
 from slumber_examples.models import Shop
@@ -33,3 +33,17 @@ class TestSlumberConfiguration(TestCase):
         self.assertEquals(rshop.web_address,
             'http://www.example.com/test/')
 
+    def test_to_json_configuration(self):
+        shop = Shop(name='Test', slug='test')
+        shop.save()
+        configure(Shop,
+            to_json = {
+                'slumber_examples.Shop.web_address':
+                    lambda r, m, i, fm, v: v.replace('http:', 'https:')
+                })
+        rshop = client.slumber_examples.Shop.get(pk=shop.pk)
+        self.assertEquals(rshop.slug, 'test')
+        self.assertEquals(rshop.web_address,
+            'https://www.example.com/test/')
+        from slumber.server.json import DATA_MAPPING
+        del DATA_MAPPING['slumber_examples.Shop.web_address']
