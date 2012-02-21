640e65b0223b20beced00482a39b1314c71bd95a
Kirit Sælensminde <k@kirit.com>
2012-02-21 10:38:50 +0700

    We can now convert from a Slumber URL back to the right URL for the local Slumber configuration.

--- slumber/scheme.py
+++ slumber/scheme.py
@@ -11,3 +11,17 @@ def to_slumber_scheme(url, service, services):
         if url.startswith(service_url):
             return 'slumber://%s/%s' % (service, url[len(service_url):])
     return url
+
+
+def from_slumber_scheme(url, service, services):
+    """Turn a Slumber URL into a full URI as specified by the service
+    configuration.
+    """
+    if url.startswith('slumber://'):
+        if services.has_key(service):
+            service_prefix = 'slumber://%s/' % service
+            if url.startswith(service_prefix):
+                service_url = services[service]
+                return service_url + url[len(service_prefix):]
+        raise NotImplementedError("Service not found")
+    return url
--- slumber_examples/tests/models.py
+++ slumber_examples/tests/models.py
@@ -1,5 +1,5 @@
 from unittest2 import TestCase
-from slumber.scheme import to_slumber_scheme
+from slumber.scheme import to_slumber_scheme, from_slumber_scheme
 
 
 class URLHandling(TestCase):
@@ -23,3 +23,30 @@ class URLHandling(TestCase):
             dict(testservice='http://example.com/slumber/testservice/'))
         self.assertEquals(translated,
             'http://www.example.com/slumber/testservice/Model/')
+
+    def test_not_a_slumber_url(self):
+        translated = from_slumber_scheme(
+            'http://example.com/slumber/', 'not_a_service',
+            dict(testservice='http://example.com/slumber/testservice/'))
+        self.assertEquals(translated,
+            'http://example.com/slumber/')
+
+    def test_slumber_service_url(self):
+        translated = from_slumber_scheme(
+            'slumber://testservice/Model/', 'testservice',
+            dict(testservice='http://example.com/slumber/testservice/'))
+        self.assertEquals(translated,
+            'http://example.com/slumber/testservice/Model/')
+
+    def test_slumber_service_url_with_a_different_service(self):
+        with self.assertRaises(NotImplementedError):
+            from_slumber_scheme(
+                'slumber://another_service/Model/', 'testservice',
+                dict(testservice='http://example.com/slumber/testservice/',
+                    another_service='http://example.com/slumber/another_service/'))
+
+    def test_slumber_service_url_with_invalid_service(self):
+        with self.assertRaises(NotImplementedError):
+            from_slumber_scheme(
+                'slumber://not-a-service/Model/', 'testservice',
+                dict(testservice='http://example.com/slumber/testservice/'))
