18e9ac742b95d706366ac48024e9f7d98a3ebd08
Kirit Sælensminde <k@kirit.com>
2012-02-22 14:37:45 +0700

    Added a RemoteForeignKey field type which can be used to handle Slumber objects running in the same or remote services.

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def read(fname):
 
 setup(
     name = "django_slumber",
-    version = "0.4.7",
+    version = "0.4.7.1",
     author = "Kirit Saelensminde",
     author_email = "kirit@felspar.com",
     url='https://github.com/KayEss/django-slumber',
