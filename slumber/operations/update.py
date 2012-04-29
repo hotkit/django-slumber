e5385359af403050fa077796a1fe364c5f6a27f4
Kirit Sælensminde <k@kirit.com>
2012-04-29 10:09:12 +0100

    Removed a dead import and tidied up some argument handling.

--- slumber/operations/update.py
+++ slumber/operations/update.py
@@ -1,8 +1,6 @@
 """
     Implements updating of instances.
 """
-from django.http import HttpResponseRedirect
-
 from slumber.operations import InstanceOperation
 from slumber.server import get_slumber_root
 from slumber.server.http import require_permission
@@ -11,10 +9,11 @@ from slumber.server.http import require_permission
 class UpdateInstance(InstanceOperation):
     """Update the attributes of a given instance.
     """
-    def post(self, request, response, appname, modelname, pk):
+    def post(self, request, response, _app, _model, pk):
         """Perform the update.
         """
-        @require_permission('%s.change_%s' % (appname, modelname.lower()))
+        @require_permission('%s.change_%s' % (
+            self.model.app.name, self.model.name.lower()))
         def do_update(_, request):
             """A function we can decorate which will allow us to use the
             decorator with a dynamic permission name.
