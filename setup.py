34453338df55969177cf6dac2cd7b8c613801bc3
Kirit Sælensminde <k@kirit.com>
2012-06-01 10:36:01 +0700

    When instances are fetched from a remote service the mock client is checked first to see if it is already holding the instance. If it is then it is used.

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ def read(fname1, fname2):
 
 setup(
     name = "django_slumber",
-    version = "0.6.1.2",
+    version = "0.6.1.3",
     author = "Kirit Saelensminde",
     author_email = "kirit@felspar.com",
     url='https://github.com/KayEss/django-slumber',
