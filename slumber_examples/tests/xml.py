381dc76267de244aa1121abfec16f72baf7c2293
siraset jirapatchandej <siraset@proteus-tech.com>
2013-11-25 17:43:51 +0700

    merge fixed conflict 0001 create xml response handler

---
+++ slumber_examples/tests/xml.py
@@ -0,0 +1,29 @@
+from __future__ import absolute_import
+
+import dicttoxml
+from xml.dom.minidom import parseString
+
+from django.http import HttpResponse
+from django.test import TestCase
+
+from slumber.server.xml import as_xml
+from slumber_examples.tests import ConfigureUser
+
+
+class TestXML(ConfigureUser, TestCase):
+    def test_as_xml(self):
+        # Arrange
+        request = {}
+        response = {'_meta': dict(status=200, message='OK')}
+        content_type = None
+
+        xml_snippet = dicttoxml.dicttoxml(response, root=False)
+        dom = parseString(xml_snippet).toprettyxml()
+
+        expected_response = HttpResponse(dom, 'text/xml', 200)
+
+        # Act
+        http_response = as_xml(request, response, content_type)
+
+        # Assert
+        self.assertEqual(str(http_response), str(expected_response))
