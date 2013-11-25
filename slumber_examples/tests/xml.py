4a9ef1f577c70e55a9ac206685781732c4c6e5c5
jiitoey <chirapat@proteus-tech.com>
2013-11-25 12:50:01 +0700

    - Only prettify xml response in DEBUG mode - return xml as root

--- slumber_examples/tests/xml.py
+++ slumber_examples/tests/xml.py
@@ -3,6 +3,7 @@ from __future__ import absolute_import
 import dicttoxml
 from xml.dom.minidom import parseString
 
+from django.conf import settings
 from django.http import HttpResponse
 from django.test import TestCase
 
@@ -11,13 +12,17 @@ from slumber_examples.tests import ConfigureUser
 
 
 class TestXML(ConfigureUser, TestCase):
+    def setUp(self):
+        settings.DEBUG = True
+        super(TestXML, self).setUp()
+
     def test_as_xml(self):
         # Arrange
         request = {}
         response = {'_meta': dict(status=200, message='OK')}
         content_type = None
 
-        xml_snippet = dicttoxml.dicttoxml(response, root=False)
+        xml_snippet = dicttoxml.dicttoxml(response, root=True)
         dom = parseString(xml_snippet).toprettyxml()
 
         expected_response = HttpResponse(dom, 'text/xml', 200)
