596d18a6106bceac3f371f2017c5bf3f6bd33e9b
Kirit Sælensminde <k@kirit.com>
2015-04-08 16:57:02 +0700

    Add in URLs.

--- test-projects/django1_8/django1_8/urls.py
+++ test-projects/django1_8/django1_8/urls.py
@@ -2,6 +2,9 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
+    (r'^slumber/', include('slumber.urls')),
+    (r'^$', 'slumber_examples.views.ok_text'),
+
     # Examples:
     # url(r'^$', 'django1_8.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
