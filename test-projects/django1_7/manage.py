5ecfd2f9b730cc1ba3d7fdf3afff998e234b9ea5
Kirit Sælensminde <k@kirit.com>
2015-04-08 17:12:22 +0700

    Allow the test code to load.

--- test-projects/django1_7/manage.py
+++ test-projects/django1_7/manage.py
@@ -3,6 +3,7 @@ import os
 import sys
 
 if __name__ == "__main__":
+    sys.path.append('../..') # Allow us to access test Django applications
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "django1_7.settings")
 
     from django.core.management import execute_from_command_line
