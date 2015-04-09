65ab0cccd02ece0ae44e4c8536a9490f8cd79604
Kirit Sælensminde <k@kirit.com>
2015-04-09 17:34:55 +0700

    Fix up the URL format.

--- test-projects/django1_8/django1_8/urls.py
+++ test-projects/django1_8/django1_8/urls.py
@@ -1,13 +1,10 @@
 from django.conf.urls import include, url
 from django.contrib import admin
 
-urlpatterns = [
-    (r'^slumber/', include('slumber.urls')),
-    (r'^$', 'slumber_examples.views.ok_text'),
 
-    # Examples:
-    # url(r'^$', 'django1_8.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
+urlpatterns = [
+    url(r'^slumber/', include('slumber.urls')),
+    url(r'^$', 'slumber_examples.views.ok_text'),
 
     url(r'^admin/', include(admin.site.urls)),
 ]
