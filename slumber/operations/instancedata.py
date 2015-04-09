026344089fddd2b0fe6686fe666307e78f33e949
Kirit Sælensminde <k@kirit.com>
2015-04-09 20:18:45 +0700

    We don't need this workaround any more.

--- slumber/operations/instancedata.py
+++ slumber/operations/instancedata.py
@@ -34,12 +34,9 @@ def instance_data(into, model, instance):
             for op in model.operations.values() if not op.model_operation])
     into['fields'] = {}
     for field, meta in model.fields.items():
-        try:
-            into['fields'][field] = dict(
-                data=to_json_data(model, instance, field, meta),
-                kind=meta['kind'], type=meta['type'])
-        except AttributeError:
-            pass
+        into['fields'][field] = dict(
+            data=to_json_data(model, instance, field, meta),
+            kind=meta['kind'], type=meta['type'])
     into['data_arrays'] = {}
     for field in model.data_arrays:
         into['data_arrays'][field] = \
