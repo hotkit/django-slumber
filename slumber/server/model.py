65e1e6cc4b557699cdf50e84903d1752b48ac259
Nui Narongwet <narongwet@proteus-tech.com>
2015-08-11 22:17:43 +0700

    Add a lookup table from database table name to slumber model

--- slumber/server/model.py
+++ slumber/server/model.py
@@ -5,7 +5,7 @@ from django.db.models import ForeignKey, ManyToManyField
 from django.db.models.fields import FieldDoesNotExist
 from django.db.models.fields.related import ManyToOneRel
 
-from slumber._caches import DJANGO_MODEL_TO_SLUMBER_MODEL
+from slumber._caches import DATABASE_TABLE_TO_SLUMBER_MODEL, DJANGO_MODEL_TO_SLUMBER_MODEL
 from slumber.operations.authenticate import AuthenticateUser
 from slumber.operations.authorization import CheckMyPermission, \
     PermissionCheck, ModulePermissions, GetPermissions
@@ -24,6 +24,7 @@ class DjangoModel(object):
     """
     # pylint: disable=too-many-instance-attributes
     def __init__(self, app, model_instance):
+        DATABASE_TABLE_TO_SLUMBER_MODEL[model_instance._meta.db_table] = self
         DJANGO_MODEL_TO_SLUMBER_MODEL[model_instance] = self
         self.app = app
         self.model = model_instance
